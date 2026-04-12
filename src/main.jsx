fab92c49859bc07f17a206464b4977af76bd1ab5
robinkowalczuk-create <robin.kowalczuk@gmail.com>
2026-04-12 22:33:59 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-<App />
-</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// PWA Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then(() => console.log("SW enregistré"))
+      .catch(err => console.log("SW erreur:", err));
+  });
+}
